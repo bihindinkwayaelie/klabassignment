58ac20b8222c63d1a1c590d7e75256c61c227e02
bihindinkwayaelie <bihindinkwayaelijah@gmail.com>
2022-12-12 09:57:34 +0200

    adding css

--- Question2/1.js
+++
@@ -1,6 +0,0 @@
-const item = [ {name: 'Bike', price:100}, {name: 'TV', price:200}, {name: 'Album', price:10}, {name: 'Book', price:5}, {name: 'Phone', price:500}, {name: 'Computer', price:1000}, ],
-
-
-const filteredItems = items.filter((item) => {
-    return item.price <= 100
-});
